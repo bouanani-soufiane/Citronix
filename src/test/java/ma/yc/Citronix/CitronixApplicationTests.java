1cc40d6f3c33f8024106b65f84af51b1e5445d46
bouanani-soufiane <soufian.bouanani2013@gmail.com>
2024-11-15 21:12:17 +0100

    Initial commit: Set up project structure and dependencies

---
+++ src/test/java/ma/yc/Citronix/CitronixApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.yc.Citronix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitronixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
