0dda59f0257f4acd761ffda2a1ed15846fd3c956
bouanani-soufiane <soufian.bouanani2013@gmail.com>
2024-11-18 22:21:14 +0100

    feat(farm): implement field retrieval by ID within a farm - Update FieldController to handle field lookup by ID - Modify FieldService to define method for fetching fields by ID - Adjust DefaultFieldService to implement field lookup logic JIRA: CIT-67

--- src/main/java/ma/yc/Citronix/farm/domain/service/FieldService.java
+++ src/main/java/ma/yc/Citronix/farm/domain/service/FieldService.java
@@ -5,6 +5,7 @@ import ma.yc.Citronix.farm.application.dto.request.create.FieldRequestDto;
 import ma.yc.Citronix.farm.application.dto.request.update.FieldUpdateDto;
 import ma.yc.Citronix.farm.application.dto.response.FieldResponseDto;
 import ma.yc.Citronix.farm.domain.model.entity.Field;
+import ma.yc.Citronix.farm.domain.model.valueObject.FieldId;
 
-public interface FieldService extends CrudService<Field, FieldRequestDto, FieldUpdateDto, FieldResponseDto> {
+public interface FieldService extends CrudService<FieldId, FieldRequestDto, FieldUpdateDto, FieldResponseDto> {
 }
