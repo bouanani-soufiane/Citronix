35c468366b7e4724a1f3d9e7bad263fadd83122c
bouanani-soufiane <soufian.bouanani2013@gmail.com>
2024-11-19 04:10:28 +0100

    feat(tree): implement tree deletion functionality - Add method to delete trees in TreeService - Update TreeController to handle tree deletion requests - Ensure proper validation and exception handling during tree deletion JIRA: CIT-69

--- src/main/java/ma/yc/Citronix/tree/api/TreeController.java
+++ src/main/java/ma/yc/Citronix/tree/api/TreeController.java
@@ -2,15 +2,14 @@ package ma.yc.Citronix.tree.api;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import ma.yc.Citronix.farm.domain.model.valueObject.FieldId;
 import ma.yc.Citronix.tree.application.dto.request.create.TreeRequestDto;
 import ma.yc.Citronix.tree.application.dto.response.TreeResponseDto;
+import ma.yc.Citronix.tree.domain.model.valueObject.TreeId;
 import ma.yc.Citronix.tree.domain.service.TreeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/trees")
@@ -23,6 +22,12 @@ class TreeController {
     public ResponseEntity<TreeResponseDto> create ( @Valid @RequestBody TreeRequestDto request ) {
         TreeResponseDto tree = service.create(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(tree);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete ( @PathVariable Long id ) {
+        service.delete(new TreeId(id));
+        return ResponseEntity.noContent().build();
     }
 
 }
