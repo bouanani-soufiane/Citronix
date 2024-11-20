f79438536ac7704c73fe21358cc82951ccf7a37a
bouanani-soufiane <soufian.bouanani2013@gmail.com>
2024-11-20 10:25:20 +0100

    feat(harvest): implement functionality to view all harvests - Add method in HarvestService to fetch all harvest records - Update HarvestController to handle requests for retrieving all harvests - Ensure proper validation and formatting of harvest data JIRA: CIT-25

--- src/main/java/ma/yc/Citronix/harvest/api/HarvestController.java
+++ src/main/java/ma/yc/Citronix/harvest/api/HarvestController.java
@@ -5,12 +5,10 @@ import lombok.RequiredArgsConstructor;
 import ma.yc.Citronix.harvest.application.dto.request.create.HarvestRequestDto;
 import ma.yc.Citronix.harvest.application.dto.response.HarvestResponseDto;
 import ma.yc.Citronix.harvest.domain.service.HarvestService;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/harvests")
@@ -19,6 +17,14 @@ class HarvestController {
 
     private final HarvestService service;
 
+    @GetMapping
+    public ResponseEntity<Page<HarvestResponseDto>> findAll (
+            @RequestParam(defaultValue = "0") Integer pageNum,
+            @RequestParam(defaultValue = "10") Integer pageSize
+    ) {
+        Page<HarvestResponseDto> harvests = service.findAll(pageNum, pageSize);
+        return ResponseEntity.ok(harvests);
+    }
 
     @PostMapping
     public ResponseEntity<HarvestResponseDto> create ( @Valid @RequestBody HarvestRequestDto request ) {
