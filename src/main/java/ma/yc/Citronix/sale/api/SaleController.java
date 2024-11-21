95dced92dd7d1e27438fe117708c0278424078e9
bouanani-soufiane <soufian.bouanani2013@gmail.com>
2024-11-21 17:09:43 +0100

    feat(sales): implement functionality to date sales - Add method in SalesService to assign and validate dates for sales - Update SalesController to handle requests for dating sales - Ensure proper validation of sales dates and data integrity JIRA: CIT-77

--- src/main/java/ma/yc/Citronix/sale/api/SaleController.java
+++ src/main/java/ma/yc/Citronix/sale/api/SaleController.java
@@ -4,13 +4,11 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import ma.yc.Citronix.sale.application.dto.request.SaleRequestDto;
 import ma.yc.Citronix.sale.application.dto.response.SaleResponseDto;
+import ma.yc.Citronix.sale.domain.model.valueObject.SaleId;
 import ma.yc.Citronix.sale.domain.service.impl.SaleService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/sales")
@@ -23,6 +21,12 @@ class SaleController {
     public ResponseEntity<SaleResponseDto> create ( @Valid @RequestBody SaleRequestDto request ) {
         SaleResponseDto sale = service.create(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(sale);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete ( @PathVariable Long id ) {
+        service.delete(new SaleId(id));
+        return ResponseEntity.noContent().build();
     }
 
 
